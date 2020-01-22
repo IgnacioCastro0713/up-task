45146165d7644bf8589e50230119234e9cc630c3
ignacio <ignaciomenchaca666@gmail.com>
2020-01-22 13:49:58 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const path  = require('path');
+const routes = require('./routes')
+
+// Template engine
+app.set('view engine', 'pug');
+
+// Path views
+app.set('views', path.join(__dirname, './views'));
+
+// Routes
+app.use(routes);
+
+app.listen(3000, () => {
+    console.log('Example app listening on port 3000!');
+});
+
+//Run app, then load http://localhost:3000 in a browser to see the output.
---
+++ routes/index.js
@@ -0,0 +1,9 @@
+const { Router } = require('express')
+
+const router = Router();
+
+const HomeController = require('../controllers/HomeController')
+
+router.get('/', HomeController.index);
+
+module.exports = router
